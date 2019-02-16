6ad20d4828f391ee83237398e47dec661a8d1182
SoumyaKoppaka <skoppaka98@gmail.com>
2019-02-16 14:39:08 +0530

    Models updated

--- roles/forms.py
+++ roles/forms.py
@@ -1,27 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import URLValidator
 from roles.models import User, Recipient, LocalBodies, BloodDonationEvent
 
 class RecipientSignUpForm(UserCreationForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput,
-        required=True
-    )
-
-    last_name = forms.CharField(
-        widget=forms.TextInput,
-        required=True
-    )
-
-    location = forms.CharField(
-        widget=forms.TextInput,
-        required=True,
-    )
-
-    email_address = forms.EmailField(
-        widget=forms.TextInput,
-        required=True
-    )
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    age = forms.CharField(required = True)
+    gender = forms.ChoiceField(choices=Recipient.GENDERS, required=True)
+    designation = forms.ChoiceField(choices=Recipient.Designation ,required=True)
+    education = forms.ChoiceField(choices=Recipient.Education, required=True)
+    income = forms.IntegerField(required=True)
 
     class Meta(UserCreationForm.Meta):
         model = User
@@ -29,31 +18,24 @@ class RecipientSignUpForm(UserCreationForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.user_type = 2
-        user.email = self.cleaned_data.get('email_address')
+        #user.email = self.cleaned_data.get('email_address')
         user.save()
         recipient = Recipient.objects.create(user=user)
         recipient.first_name = self.cleaned_data.get('first_name')
         recipient.last_name = self.cleaned_data.get('last_name')
-        recipient.location = self.cleaned_data.get('location')
+        recipient.age = self.cleaned_data.get('age')
+        recipient.education=self.cleaned_data.get('education')
+        recipient.gender=self.cleaned_data.get('gender')
+        recipient.designation=self.cleaned_data.get('designation')
         recipient.save()
         return user
 
 
 class LocalBodySignUpForm(UserCreationForm):
-    name = forms.CharField(
-        widget=forms.TextInput,
-        required=True
-    )
-
-    location = forms.CharField(
-        widget=forms.TextInput,
-        required=True,
-    )
-
-    email_address = forms.EmailField(
-        widget=forms.TextInput,
-        required=True
-    )
+    name = forms.CharField(required=True)
+    location = forms.CharField(required=True)
+    email_address = forms.EmailField(required=True)
+    contact = forms.CharField(required=True)
 
     class Meta(UserCreationForm.Meta):
         model = User
@@ -64,20 +46,26 @@ class LocalBodySignUpForm(UserCreationForm):
         user.email = self.cleaned_data.get('email_address')
         user.save()
         local_body = LocalBodies.objects.create(user=user)
+        local_body.name=self.cleaned_data.get('name')
+        local_body.location=self.cleaned_data.get('location')
+        local_body.email_address=self.cleaned_data.get('email')
+        local_body.contact=self.cleaned_data.get('contact')
         local_body.save()
         return user
 
 class EventForm(forms.ModelForm):
-    date = forms.DateField(
-        widget=forms.DateInput,
-        required=True
-    )
-
-    description = forms.CharField(
-        widget=forms.Textarea,
-        required=True
-    )
+    name = forms.CharField(required=True)
+    organizer = forms.CharField(required=True)
+    contact = forms.CharField(required=True)
+    description = forms.CharField(required=True)
+    el_age = forms.CharField(required=False)
+    el_designation = forms.ChoiceField(choices=BloodDonationEvent.Designation, required=True)
+    el_education = forms.ChoiceField(choices=BloodDonationEvent.Education, required=True)
+    el_income = forms.IntegerField(required=False)
+    el_gender = forms.ChoiceField(choices=BloodDonationEvent.GENDERS, required=False)
+    link = forms.URLField()
+    poster = forms.FileField(required=False)
 
     class Meta:
         model = BloodDonationEvent
-        fields = ['name', 'organizer', 'location', 'date', 'description']
+        fields = ['name', 'organizer','contact','description','el_age','el_designation','el_education','el_income','el_gender','link','poster']
--- roles/models.py
+++ roles/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+#from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import URLValidator
 from django.db import models
 import datetime
 
@@ -10,12 +12,43 @@ class User(AbstractUser):
     )
 
     user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, default=6)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, default='')
 
 class Recipient(models.Model):
     first_name = models.CharField(max_length=40, default='')
     last_name = models.CharField(max_length=40, default='')
-    location = models.CharField(max_length=40, default='')
+    #location = models.CharField(max_length=40, default='')
+
+    age = models.CharField(max_length=2, default='')
+
+    Education = (
+        ('Uneducated', 'u'),
+        ('School', 'sch'),
+        ('SSC', 'ssc'),
+        ('HSC', 'hsc'),
+        ('Graduate', 'grad'),
+        ('Post Graduate', 'postgrad'),
+    )
+    education = models.CharField(max_length=13, choices=Education, default='Uneducated')
+
+    Designation = (
+        ('Govt. Employee', 'Govt_Employee'),
+        ('Businessman', 'Business'),
+        ('Private Employee', 'Private_Employee'),
+        ('Unemployed', 'Unemployed'),
+        ('Others', 'Others'),
+    )
+    designation = models.CharField(max_length=16, choices=Designation, default='Others')
+    income = models.IntegerField(default=0)
+    GENDER_MALE = 'M'
+    GENDER_FEMALE = 'F'
+    GENDER_OTHER = 'O'
+    GENDERS = (
+        (GENDER_MALE, 'Male'),
+        (GENDER_FEMALE, 'Female'),
+        (GENDER_OTHER, 'Other'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDERS, default='O')
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
@@ -25,19 +58,48 @@ class LocalBodies(models.Model):
     name = models.CharField(max_length=40, default='')
     location = models.CharField(max_length=40, default='')
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-
+    contact = models.CharField(max_length=10, default='')
+    email = models.EmailField(default='')
     def __str__(self):
         return self.user.username
 
 
 class BloodDonationEvent(models.Model):
     user = models.ForeignKey(User, default=5, on_delete=models.CASCADE)
-    name = models.CharField(max_length=250)
-    organizer = models.CharField(max_length=50)
-    date = models.DateTimeField(default=datetime.datetime.now())
-    location = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
-    # time=models.TimeField()
+    name = models.CharField(max_length=250, default='')
+    organizer = models.CharField(max_length=50, default='')
+    contact = models.CharField(max_length=10,default='')
+    description = models.CharField(max_length=500, default='')
+    el_age = models.CharField(max_length=2, default='')
+    Education = (
+        ('Uneducated', 'u'),
+        ('School', 'sch'),
+        ('SSC', 'ssc'),
+        ('HSC', 'hsc'),
+        ('Graduate', 'grad'),
+        ('Post Graduate', 'postgrad'),
+    )
+    el_education = models.CharField(max_length=13, choices=Education, default='Uneducated')
+
+    Designation = (
+        ('Govt. Employee', 'Govt_Employee'),
+        ('Businessman', 'Business'),
+        ('Private Employee', 'Private_Employee'),
+        ('Unemployed', 'Unemployed'),
+        ('Others', 'Others'),
+    )
+    el_designation = models.CharField(max_length=16, choices=Designation, default='Others')
+    el_income = models.IntegerField(default=0)
+    GENDER_MALE = 'M'
+    GENDER_FEMALE = 'F'
+    GENDER_OTHER = 'O'
+    GENDERS = (
+        (GENDER_MALE, 'Male'),
+        (GENDER_FEMALE, 'Female'),
+        (GENDER_OTHER, 'Other'),
+    )
+    el_gender = models.CharField(max_length=1, choices=GENDERS, default='O')
+    link = models.TextField(validators=[URLValidator()], default='')
     poster = models.FileField(null=True,blank=True)
     status = models.IntegerField(default=0)
 
